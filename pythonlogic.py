b0b109371d164254259a6366c59f39129b0ce9a2
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-03-18 22:40:23 +0530

    new update

---
+++ pythonlogic.py
@@ -0,0 +1,14 @@
+def greet(name):
+
+    return f"hello {name}"
+
+print(greet("raksh"))
+
+
+# git add .
+# git commit -m "new update"
+# git push
+
+
+#we cover if else aand loop logic bulding question around 50 question
+
